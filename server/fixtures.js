25d28488e83cc0b1fa2a5c5b6b73ca095942ff16
Yanis <yanis.ikene.1@etsmtl.net>
2015-03-10 01:44:13 -0400

    added jqxWidget, gauge not rendering...

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,9 +1,9 @@
 Meteor.startup(function () {
 
-    /*
+
     Meteor.setInterval(function(){
-        MotorTemperatures.insert({value: _.random(0, 150) , timestamp : Date.now()});
-    }, 1000)
-    */
+        MotorTemperatures.insert({value: _.random(0, 100) , timestamp : Date.now()});
+    }, 2000)
+
 
   });
